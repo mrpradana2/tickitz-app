151a57662bd95cb2d143c5d3ab71da0e80e248df
mrpradana2 <mrpradana2@gmail.com>
2025-03-12 23:32:28 +0700

    feat: Add header for web page

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import "./index.css";
-// import App from "./App.jsx";
 import "./style/tailwind.css";
-import Home from "./pages/Home/Home.jsx";
+import Header from "./components/Header";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Home />
-    {/* <App /> */}
+    <Header />
   </StrictMode>
 );
