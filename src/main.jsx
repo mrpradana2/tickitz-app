3c9cb6ed687e933c7f2d4e3a1d05d21b727f548b
mrpradana2 <mrpradana2@gmail.com>
2025-03-15 15:09:00 +0700

    fix: adding router ib the main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import Router from "./router.jsx";
-// import { BrowserRouter } from "react-router";
 import "./style/tailwind.css";
-// import App from "./App.jsx";
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
-import Home from "./pages/home/Home";
+import Router from "./router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Home />
+    <Router />
   </StrictMode>
 );
