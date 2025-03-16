da55f7fd51f599af4a6d1dae05e8c435fa87387c
mrpradana2 <mrpradana2@gmail.com>
2025-03-16 12:50:43 +0700

    feat: add hero section for details page

---
+++ src/components/detailsMovie/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HeroDetails() {
+  return (
+    <>
+      <section className="h-[300px] w-full relative after:content-[''] after:absolute after:top-0 after:left-0 after:right-0 after:bottom-0 after:bg-[url('/images/hero/hero-details.jpg')] after:bg-cover after:bg-center after:brightness-[0.45]"></section>
+    </>
+  );
+}
+
+export default HeroDetails;
