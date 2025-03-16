2567c6f93c47926fd55df4e22c47ee6af512d1af
mrpradana2 <mrpradana2@gmail.com>
2025-03-16 12:15:14 +0700

    fix: fix path directory import

--- src/pages/movie/ListMovie.jsx
+++ src/pages/movie/ListMovie.jsx
@@ -1,6 +1,6 @@
-import LayoutMovieList from "../../../layouts/LayoutMovieList";
-import HeroSection from "../../../components/listMovie/Hero";
-import ListMoviesSection from "../../../components/listMovie/ListMovies";
+import LayoutMovieList from "../../layouts/LayoutMovieList";
+import HeroSection from "../../components/listMovie/Hero";
+import ListMoviesSection from "../../components/listMovie/ListMovies";
 
 function ListMovie() {
   return (
