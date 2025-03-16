884c98dc2df9d8a70850aee0db04aec07cfd7316
mrpradana2 <mrpradana2@gmail.com>
2025-03-16 11:13:14 +0700

    feat: create list movie page

---
+++ src/pages/ListMovie.jsx
@@ -0,0 +1,20 @@
+import LayoutMovieList from "../layouts/LayoutMovieList";
+import HeroSection from "../components/listMovie/Hero";
+import ListMoviesSection from "../components/listMovie/ListMovies";
+
+function ListMovie() {
+  return (
+    <>
+      <LayoutMovieList
+        content={
+          <>
+            <HeroSection />
+            <ListMoviesSection />
+          </>
+        }
+      />
+    </>
+  );
+}
+
+export default ListMovie;
