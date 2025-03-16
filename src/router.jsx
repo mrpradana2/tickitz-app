263e748b4b38a10ec0bf69f3bc01f783a7feb152
mrpradana2 <mrpradana2@gmail.com>
2025-03-16 12:14:26 +0700

    feat: add router for details page

--- src/router.jsx
+++ src/router.jsx
@@ -3,7 +3,8 @@ import Signup from "./pages/auth/signin";
 import Signin from "./pages/auth/signup";
 import Home from "./pages/Home";
 import Section from "./components/listMovie/Hero";
-import ListMovies from "./pages/ListMovie";
+import ListMovies from "./pages/movie/ListMovie";
+import Details from "./pages/movie/Details";
 import React from "react";
 
 export default function Router() {
@@ -15,7 +16,10 @@ export default function Router() {
           <Route path="signin" element={<Signin />} />
           <Route path="signup" element={<Signup />} />
         </Route>
-        <Route path="/movie" element={<ListMovies />} />
+        <Route path="movie">
+          <Route index element={<ListMovies />} />
+          <Route path="details" element={<Details />} />
+        </Route>
         <Route path="/cek" element={<Section />} />
       </Routes>
     </BrowserRouter>
