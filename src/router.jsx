26d265ab2fc70196d81bb6dc32792b78dab09c7a
mrpradana2 <mrpradana2@gmail.com>
2025-03-17 06:23:06 +0700

    fix: delete route for debugging

--- src/router.jsx
+++ src/router.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import Signup from "./pages/auth/signin";
 import Signin from "./pages/auth/signup";
 import Home from "./pages/Home";
-import Section from "./components/listMovie/Hero";
 import ListMovies from "./pages/movie/ListMovie";
 import Details from "./pages/movie/Details";
 import React from "react";
@@ -20,7 +19,6 @@ export default function Router() {
           <Route index element={<ListMovies />} />
           <Route path="details" element={<Details />} />
         </Route>
-        <Route path="/cek" element={<Section />} />
       </Routes>
     </BrowserRouter>
   );
