42b7b7259e9f7f05d8cd6a7f7f319bc302116430
mrpradana2 <mrpradana2@gmail.com>
2025-03-20 12:16:45 +0700

    feat: add route details page, order page, payment page, ticket result page, and private route for list movie

--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,15 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Signin from "./pages/auth/signin";
 import Signup from "./pages/auth/signup";
 import Home from "./pages/Home";
 import ListMovies from "./pages/movie/ListMovie";
 import Details from "./pages/movie/Details";
-import React from "react";
+import OrderPage from "./pages/OrderPage";
+import TicketResult from "./pages/TicketResult";
+import Payment from "./pages/PaymentPage";
+import PrivateRoute from "./components/PrivateRoute";
+// import userProvider from "./contexts/userContext";
 
 export default function Router() {
   return (
@@ -16,8 +21,18 @@ export default function Router() {
           <Route path="signup" element={<Signup />} />
         </Route>
         <Route path="movie">
-          <Route index element={<ListMovies />} />
+          <Route
+            index
+            element={
+              <PrivateRoute redirectTo="/auth/signin">
+                <ListMovies />
+              </PrivateRoute>
+            }
+          />
           <Route path="details" element={<Details />} />
+          <Route path="order" element={<OrderPage />} />
+          <Route path="payment" element={<Payment />} />
+          <Route path="ticket-result" element={<TicketResult />} />
         </Route>
       </Routes>
     </BrowserRouter>
