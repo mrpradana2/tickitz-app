ce0200cd5d5a9f6a9d1b73a8c76bb3425547dbaa
mrpradana2 <mrpradana2@gmail.com>
2025-03-20 12:08:22 +0700

    add: add file to configuration user context

---
+++ src/contexts/userContext.jsx
@@ -0,0 +1,26 @@
+import { createContext } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
+
+const initialUser = {
+  email: "",
+  password: "",
+};
+
+export const userContext = createContext({
+  user: initialUser,
+  setUser: () => {},
+});
+
+export default function userProvider({ childern }) {
+  const [user, setUser] = useLocalStorage("account:user", initialUser);
+  return (
+    <userContext.Provider
+      value={{
+        user,
+        setUser,
+      }}
+    >
+      {childern}
+    </userContext.Provider>
+  );
+}
