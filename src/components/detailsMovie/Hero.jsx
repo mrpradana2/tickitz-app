46ea5e8ea562f05ce9c235a09329cb33535a0df6
mrpradana2 <mrpradana2@gmail.com>
2025-03-22 20:32:30 +0700

    feat: added image from fetch data

--- src/components/detailsMovie/Hero.jsx
+++ src/components/detailsMovie/Hero.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router";
+const VITE_AUTH = import.meta.env.VITE_AUTH;
 
 function HeroDetails() {
+  const [detailsMovie, setDetailsMovie] = useState({});
+  const { id } = useParams();
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${VITE_AUTH}`,
+    },
+  };
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${id}?language=en-US`, options)
+      .then((res) => res.json())
+      .then((res) => setDetailsMovie(res))
+      .catch((err) => console.error(err));
+  }, []);
+
+  console.log(detailsMovie);
   return (
     <>
-      <section className="h-[300px] w-full relative after:content-[''] after:absolute after:top-0 after:left-0 after:right-0 after:bottom-0 after:bg-[url('/images/hero/hero-details.jpg')] after:bg-cover after:bg-center after:brightness-[0.45]"></section>
+      <section className={`h-[300px] w-full relative overflow-hidden`}>
+        <img
+          src={`https://image.tmdb.org/t/p/w500/${detailsMovie.backdrop_path}`}
+          alt="backdrop"
+          className="w-full brightness-50"
+        />
+      </section>
     </>
   );
 }
