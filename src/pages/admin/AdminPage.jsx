d199bc2800d68c588141361404a228572edaad0f
mrpradana2 <mrpradana2@gmail.com>
2025-03-23 11:35:38 +0700

    feat: add state for navigation

--- src/pages/admin/AdminPage.jsx
+++ src/pages/admin/AdminPage.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import { useState } from "react";
 import LayoutHeaderAdmin from "../../layouts/LayoutHeaderAdmin";
 import MovieListAdmin from "../../components/admin/MovieListAdmin";
+import ChartDashboard from "../../components/admin/ChartDashboard";
 
 export default function AdminPage() {
+  const [navigation, setNavgation] = useState(true);
+  function handleNav1() {
+    setNavgation(true);
+  }
+
+  function handleNav2() {
+    setNavgation(false);
+  }
+
   return (
     <>
-      <LayoutHeaderAdmin content={<MovieListAdmin />} />
+      <LayoutHeaderAdmin
+        onClickNav1={handleNav1}
+        onClickNav2={handleNav2}
+        navigation={navigation}
+        content={
+          <>
+            <MovieListAdmin navigation={navigation} />
+            <ChartDashboard navigation={navigation} />
+          </>
+        }
+      />
     </>
   );
 }
